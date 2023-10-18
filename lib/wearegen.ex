a5fe8a34f5d71c7786dc7612b7de5208bb3f2d99
Denis Makula <denis@emdeevy.dev>
2023-10-18 22:26:18 +0000

    initial commit

---
+++ lib/wearegen.ex
@@ -0,0 +1,18 @@
+defmodule Wearegen do
+  @moduledoc """
+  Documentation for `Wearegen`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wearegen.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/wearegen_test.exs
@@ -0,0 +1,8 @@
+defmodule WearegenTest do
+  use ExUnit.Case
+  doctest Wearegen
+
+  test "greets the world" do
+    assert Wearegen.hello() == :world
+  end
+end
